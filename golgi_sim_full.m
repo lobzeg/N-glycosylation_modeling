c4d3aef1fe774208f3e13a32dcaba6706d99f2f7
Artem Krantsevich <32847572+lobzeg@users.noreply.github.com>
2018-03-16 16:13:34 -0400

    comments added, names of some variables changed

--- golgi_sim_full.m
+++ golgi_sim_full.m
@@ -1,13 +1,16 @@
-function [a] = golgi_sim_full(a,s,Vm,Km,ChangeCompartmentRate,concentration,NE)
+function [a] = golgi_sim_full(a,s,v_m,k_m,change_compartment_rate,concentration,n_e)
 
 compartment = a(11);
 sia = 0;
 
+% Perform reactions until reaction "leave 4th ompartment" happens
 while compartment < 5
     
-    [e, MM, Msum] = available_reactions(a,s,Vm,Km,ChangeCompartmentRate,concentration,NE,sia);
+    % Determine possible reactions and likelihood of each of them
+    [e, m_m, m_sum] = available_reactions(a,s,v_m,k_m,change_compartment_rate,concentration,n_e,sia);
 
-    [a,sia] = perform_reaction(MM,Msum,a,sia,NE);
+    % Perform a random reaction from available ones
+    [a,sia] = perform_reaction(m_m,m_sum,a,sia,n_e);
     compartment=a(11);
 end;
 end
